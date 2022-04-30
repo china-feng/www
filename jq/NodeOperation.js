e06edf3ee5844719e506d156af1751709e171847
IpQuery <31477408+china-feng@users.noreply.github.com>
2022-04-30 21:13:40 +0800

    Create NodeOperation.js

---
+++ jq/NodeOperation.js
@@ -0,0 +1,9 @@
+// jq 对象结点操作
+$(jsObject); //将js对象转换成jq对象; 反之：var jsObject = jqObject[0]
+
+jqObject.attr();  //设置属性 获取属性值
+
+jqObject.removeClass(); //删除某个类名
+jqObject.addClass(); //增加某个类名
+
+jqObject.each(function (index, item) {} )
